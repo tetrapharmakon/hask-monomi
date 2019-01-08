55e18a3ed84373a3e36be2e1136f9fd677d44459
fouche <tetrapharmakon@gmail.com>
2019-01-08 10:49:43 +0100

    refactoring a bit

--- Multinomiala.hs
+++ Multinomiala.hs
@@ -6,6 +6,10 @@ import           Data.Either()
 type Monomial = (Integer, [(Char, Int)])
 type Polynomial = [Monomial]
 
+
+-- polyRaise
+-- polynomial substitution?
+
 -- groups similar elements
 pack :: (Eq a) => [a] -> [[a]]
 pack []       = []
@@ -18,8 +22,11 @@ rleCompress y@(x : xs) =
  (x, length $ head $ pack y) : rleCompress (dropWhile (== x) xs)
 
 rleReduce :: (Eq a, Ord a) => [(a, Int)] -> [(a, Int)]
-rleReduce a = filter (\x -> snd x /= 0) $ map reducer $ groupBy (\x y -> fst x == fst y) $ sortOn fst a
- where reducer w = (fst $ head w, sum $ map snd w)
+rleReduce a = filter appears $ map reducer $ groupBy alike $ sortOn fst a
+ where
+   appears x = snd x /= 0
+   alike x y = fst x == fst y 
+   reducer w = (fst $ head w, sum $ map snd w)
 
 rleExpandPiece :: (a, Int) -> [a]
 rleExpandPiece (a, n) = map (const a) [1 .. n]
@@ -43,10 +50,6 @@ monoMultiply p q = monoReduce (fst p * fst q, snd p ++ snd q)
 -- will be a useful sort key to reduce polynomials), 
 isMultiple :: Monomial -> Monomial -> Bool 
 isMultiple m n = sort (snd m) == sort (snd n)
--- isLike (tells if a monomial has same indets, and possibly different exponents;
--- not sure if this is needed?)
-isLike :: Monomial -> Monomial -> Bool 
-isLike m n = sort (monoExtractVar m) == sort (monoExtractVar n)
 
 -- extract variables
 monoExtractVar :: Monomial -> String
@@ -104,14 +107,19 @@ monoProduct ms = monoReduce $ foldr monoProduct' (1,[]) $ map monoReduce ms
 
 -- reduce a polynomial
 polyReduce :: Polynomial -> Polynomial
-polyReduce p = filter (\x -> fst x /= 0) $ map monoSum $ groupBy (\x y -> isMultiple x y) $ sortOn snd p
+polyReduce p = filter nonzero $ map monoSum $ groupBy alike $ sortOn snd p
+  where
+    nonzero x = fst x /= 0
+    alike x y = isMultiple x y
 
 -- sum two polynomials
 polySum' :: Polynomial -> Polynomial -> Polynomial
 polySum' p q = polyReduce (p ++ q)
 
 polySum :: [Polynomial] -> Polynomial
-polySum ps = polyReduce $ foldl polySum' [(0,[])] ps
+polySum ps = polyReduce $ foldl polySum' [(0,[])] rps
+  where
+    rps = map polyReduce ps
 
 polyProduct' :: Polynomial -> Polynomial -> Polynomial
 polyProduct' p q = polyReduce $ concat [[monoProduct' m n | n <- p] | m <- q]
