bfd6b02b5b9da3c0513c567708866633b92b9291
fouche <tetrapharmakon@gmail.com>
2019-01-09 02:58:26 +0100

    refactoring quickspeck

--- QuickSpeck.hs
+++ QuickSpeck.hs
@@ -11,23 +11,27 @@ import Test.Hspec.Core.QuickCheck (modifyMaxSuccess)
 main :: IO ()
 main = hspec $ 
   describe "Multinomiala" $ do
-  modifyMaxSuccess (const 100) $ it "monoInit is inverse to extract vars and exps" $ property $ 
+  modifyMaxSuccess (const 100) $ it "monoInit is inverse to extract vars and exps" $ property 
     (\p -> monoInit (fst p) (monoExtractVar p) (monoExtractExps p) == monoReduce (p :: Monomial))
-    -- prop_SumIsAssociative
-  -- modifyMaxSuccess (const 100) $ it "polySum is associative" $ property $
-  --   (\a b c -> polySum' (polySum' (a :: Polynomial) (b :: Polynomial)) (c :: Polynomial) == polySum' a (polySum' b c) )
+-- prop_SumIsAssociative
+  modifyMaxSuccess (const 100) $ it "polySum is associative" $ property
+    (\a b c -> polySum' (polySum' (a :: Polynomial) (b :: Polynomial)) (c :: Polynomial) == polySum' a (polySum' b c) )
 -- prop_ProdIsAssociative
-  -- modifyMaxSuccess (const 100) $ it "monoProduct is associative" $ property $
-  --   (\a b c -> monoProduct' (monoProduct' (a :: Monomial) (b :: Monomial)) (c :: Monomial) == monoProduct' a (monoProduct' b c) )
+  modifyMaxSuccess (const 100) $ it "monoProduct is associative" $ property
+    (\a b c -> monoProduct' (monoProduct' (a :: Monomial) (b :: Monomial)) (c :: Monomial) == monoProduct' a (monoProduct' b c) )
 -- -- prop_SumIsCommutative
-  -- modifyMaxSuccess (const 100) $ it "polySum is commutative" $ property $
-  --   (\a b -> polySum' (a :: Polynomial) (b :: Polynomial) == polySum' b a)
+  modifyMaxSuccess (const 100) $ it "polySum is commutative" $ property
+    (\a b -> polySum' (a :: Polynomial) (b :: Polynomial) == polySum' b a)
 -- -- prop_ProdIsCommutative
-  -- modifyMaxSuccess (const 100) $ it "monoProduct is commutative" $ property $
-  --   (\a b -> monoProduct' (a :: Monomial) (b :: Monomial) == monoProduct' b a)
+  modifyMaxSuccess (const 100) $ it "monoProduct is commutative" $ property
+    (\a b -> monoProduct' (a :: Monomial) (b :: Monomial) == monoProduct' b a)
 -- -- prop_ProdDistributes
-  -- modifyMaxSuccess (const 100) $ it "polySum is associative" $ property $
-  --   (\a b c -> polySum' (polySum' (a :: Polynomial) (b :: Polynomial)) (c :: Polynomial) == polySum' a (polySum' b c) )
-  modifyMaxSuccess (const 100) $ it "polyInit coincides with monoInit on singletons" $ property $
+  modifyMaxSuccess (const 100) $ it "polyProduct is distributive" $ property
+    (
+      \a b c -> polySum' (polyProduct' (a :: Polynomial) (b :: Polynomial)) (polyProduct' a (c :: Polynomial)) == 
+      polyProduct' a (polySum' b c)    
+    )
+-- initialization of polynomials vs monomials
+  modifyMaxSuccess (const 100) $ it "polyInit coincides with monoInit on singletons" $ property
     (\n s d -> [monoInit (n :: Integer) (s :: String) (d :: [Int])] == polyInit [(n, s, d)])
 -- polyRaise [p] n == monoRaise p n
