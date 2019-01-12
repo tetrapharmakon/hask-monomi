ea8086fa2d01d209d5aacfa12f7f7220b0d1a906
fouche <tetrapharmakon@gmail.com>
2019-01-12 02:45:56 +0100

    hindent on main file

--- Multinomiala.hs
+++ Multinomiala.hs
@@ -1,29 +1,31 @@
 module Multinomiala where
 
-import           Data.List
-import           Data.Either()
+import Data.Either ()
+import Data.List
+import Data.Maybe ()
 
 type Monomial = (Integer, [(Char, Int)])
+
 type Polynomial = [Monomial]
 
 -- groups similar elements
 pack :: (Eq a) => [a] -> [[a]]
-pack []       = []
-pack (x : xs) = (x : takeWhile (== x) xs) : pack (dropWhile (== x) xs)
+pack [] = []
+pack (x:xs) = (x : takeWhile (== x) xs) : pack (dropWhile (== x) xs)
 
 -- a few RLE utils
 rleCompress :: Eq a => [a] -> [(a, Int)]
 rleCompress [] = []
-rleCompress y@(x : xs) =
- (x, length $ head $ pack y) : rleCompress (dropWhile (== x) xs)
+rleCompress y@(x:xs) =
+  (x, length $ head $ pack y) : rleCompress (dropWhile (== x) xs)
 
 rleReduce :: (Eq a, Ord a) => [(a, Int)] -> [(a, Int)]
 rleReduce [] = []
-rleReduce  a = filter appears $ map reducer $ groupBy alike $ sortOn fst a
- where
-   appears x = snd x /= 0
-   alike x y = fst x == fst y 
-   reducer w = (fst $ head w, sum $ map snd w)
+rleReduce a = filter appears $ map reducer $ groupBy alike $ sortOn fst a
+  where
+    appears x = snd x /= 0
+    alike x y = fst x == fst y
+    reducer w = (fst $ head w, sum $ map snd w)
 
 rleExpandPiece :: (a, Int) -> [a]
 rleExpandPiece (a, n) = map (const a) [1 .. n]
@@ -36,8 +38,9 @@ rleAddToList ls x = rleReduce $ rleCompress (ls ++ rleExpand x)
 
 -- reduction of a monomial
 monoReduce :: Monomial -> Monomial
-monoReduce m | fst m == 0 = (0,[])
-             | otherwise  = (fst m, rleReduce (snd m))
+monoReduce m
+  | fst m == 0 = (0, [])
+  | otherwise = (fst m, rleReduce (snd m))
 
 -- algebra with monomials: multiplication
 monoMultiply :: Monomial -> Monomial -> Monomial
@@ -45,7 +48,7 @@ monoMultiply p q = monoReduce (fst p * fst q, snd p ++ snd q)
 
 -- isMultiple (tells if a monomial is scalar multiple of another;
 -- will be a useful sort key to reduce polynomials), 
-isMultiple :: Monomial -> Monomial -> Bool 
+isMultiple :: Monomial -> Monomial -> Bool
 isMultiple m n = sort (snd m) == sort (snd n)
 
 -- extract variables
@@ -68,24 +71,28 @@ polyConst :: Integer -> Polynomial
 polyConst n = [monoConst n]
 
 -- show polynomial
-monoJoin :: (Char,Int) -> String
-monoJoin (x,e) | e == 0 = ""
-               | e == 1 = [x]
-               | e > 1  = [x] ++ "^" ++ show e
-               | otherwise  = [x] ++ "^{" ++ show e ++ "}"
+monoJoin :: (Char, Int) -> String
+monoJoin (x, e)
+  | e == 0 = ""
+  | e == 1 = [x]
+  | e > 1 = [x] ++ "^" ++ show e
+  | otherwise = [x] ++ "^{" ++ show e ++ "}"
 
 monoShow :: Monomial -> String
-monoShow m 
-  | fst m == 1 = if null (snd rm) then "1" else concatMap monoJoin (snd rm)
+monoShow m
+  | fst m == 1 =
+    if null (snd rm)
+      then "1"
+      else concatMap monoJoin (snd rm)
   | otherwise = show (fst rm) ++ concatMap monoJoin (snd rm)
-    where
-      rm = monoReduce m
+  where
+    rm = monoReduce m
 
 -- raise a monomial to the exp-power 
-monoRaise :: Monomial -> Int -> Monomial 
+monoRaise :: Monomial -> Int -> Monomial
 monoRaise m e = monoReduce (fst m ^ e, map (raiser e) (snd m))
   where
-    raiser n z = (fst z,n * snd z)
+    raiser n z = (fst z, n * snd z)
 
 -- fix it:
 -- monoSum :: Monomial -> Monomial -> Either Polynomial Monomial
@@ -93,15 +100,13 @@ monoRaise m e = monoReduce (fst m ^ e, map (raiser e) (snd m))
 --             | otherwise      = Left [m,n]
 -- is more elegant.
 -- Currently, monoSum _assumes_ that its arguments are summable
-
 -- sum two Monomials
 monoSum' :: Monomial -> Monomial -> Monomial
 --this is very inappropriate and should be changed!
-monoSum' m n = monoReduce (fst m + fst n,snd m) 
+monoSum' m n = monoReduce (fst m + fst n, snd m)
 
 monoSum :: [Monomial] -> Monomial
--- monoSum [] = (0, [])
-monoSum ms = monoReduce $ foldl monoSum' (0,snd $ head rms) rms
+monoSum ms = monoReduce $ foldl monoSum' (0, snd $ head rms) rms
   where
     rms = map monoReduce ms
 
@@ -119,16 +124,16 @@ polyReduce p = filter nonzero $ map monoSum $ groupBy isMultiple $ sortOn snd rp
     rp = map monoReduce p
 
 polyInit :: [(Integer, String, [Int])] -> Polynomial
-polyInit = map monoPInit
+polyInit = map tupleInit
   where
-    monoPInit (x, y, z) = monoInit x y z
+    tupleInit (x, y, z) = monoInit x y z
 
 -- sum two polynomials
 polySum' :: Polynomial -> Polynomial -> Polynomial
 polySum' p q = polyReduce (polyReduce p ++ polyReduce q)
 
 polySum :: [Polynomial] -> Polynomial
-polySum ps = polyReduce $ foldl polySum' [(0,[])] rps
+polySum ps = polyReduce $ foldl polySum' [(0, [])] rps
   where
     rps = map polyReduce ps
 
@@ -136,7 +141,7 @@ polyProduct' :: Polynomial -> Polynomial -> Polynomial
 polyProduct' p q = polyReduce $ concat [[monoProduct' m n | n <- p] | m <- q]
 
 polyProduct :: [Polynomial] -> Polynomial
-polyProduct ps = polyReduce $ foldr polyProduct' [(1,[])] ps
+polyProduct ps = polyReduce $ foldr polyProduct' [(1, [])] ps
 
 -- show a polynomial
 polyShow :: Polynomial -> String
